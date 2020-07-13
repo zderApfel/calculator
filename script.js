97d7852377fad220c6699d366d9cc4a3e655ba26
zderApfel <scoutt172@yahoo.com>
2020-07-13 18:25:51 -0400

    Pseudocode for javascript

--- script.js
+++ script.js
@@ -13,17 +13,28 @@ How it will be done:
     - Second value
     - result
 - Declare a screen variable that keeps changing as values and operators are added
-Take input from event listeners on the number keys and adds into the first value of the Calculation, put this value on the screen
-Take input from the event listenrs on the operator keys
-Take input from the number keys and adds it into the second value, put this value on the screen as well
+-Take input from event listeners on the number keys and adds into the first value of the Calculation, put this value on the screen
+-Take input from the event listenrs on the operator keys
+-Take input from the number keys and adds it into the second value, put this value on the screen as well
+
 if (equals sign is pressed){
+    if (division sign is operator && second value == 0){
+        display MATH ERROR on screen
+        break;
+    }
+    else if (operation > 9999999){
+        display OVERFLOW ERROR
+        break;
+    }
     display the result on screen and put it in the Calculation object
 }
-    else if (operator sign is pressed){
-        replace the first value with the result value
-        clear second value
-        await second value
-        continue until broken
+else if (operator sign is pressed){
+    replace the first value with the result value
+    clear second value
+    await second value
+    continue until broken
 }
-Erase button erases 
+
+
+Erase button erases all stored values and starts fresh
 */

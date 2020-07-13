9ced6027768492c075d59601c40ee636781fb0f4
zderApfel <scoutt172@yahoo.com>
2020-07-13 18:21:51 -0400

    Began pseudocode for javascript

---
+++ script.js
@@ -0,0 +1,29 @@
+/*
+--CALCULATOR PROJECT--
+
+This is it Zach. The final project of Web Development 101. You can do this! The calulator must do the following:
+
+Goal: Be able to run all basic four functions of a calculator (addition, subtraction, multiplication, and division)
+
+How it will be done:
+
+- Declare an "object" (named Calculation) with three properties: 
+    - First value
+    - Operator
+    - Second value
+    - result
+- Declare a screen variable that keeps changing as values and operators are added
+Take input from event listeners on the number keys and adds into the first value of the Calculation, put this value on the screen
+Take input from the event listenrs on the operator keys
+Take input from the number keys and adds it into the second value, put this value on the screen as well
+if (equals sign is pressed){
+    display the result on screen and put it in the Calculation object
+}
+    else if (operator sign is pressed){
+        replace the first value with the result value
+        clear second value
+        await second value
+        continue until broken
+}
+Erase button erases 
+*/

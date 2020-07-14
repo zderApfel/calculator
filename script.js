ecede2163205076bff3824e76d72e9050bec9d57
zderApfel <scoutt172@yahoo.com>
2020-07-13 23:33:23 -0400

    Number keys now add a number to screen

--- script.js
+++ script.js
@@ -42,16 +42,34 @@ Erase button erases all stored values and starts fresh
 const SCREEN = document.getElementById("screen");
 const BUTTON_AREA = document.getElementById("button-grid");
 const ERASE_BUTTON = document.getElementById("erase-button");
+//Add event listener for ERASE_BUTTON which will reset all values
 
 initialize();
 
-function initialize(){
+function initialize(){ //Function creates buttons, and gives them their appropriate functions
+    SCREEN.value = 0;
     for (i=1; i <= 16; i++){
         let symbolArray = "789+456-123*.0=÷".split("");
         let button = document.createElement("div");
         BUTTON_AREA.appendChild(button);
         button.classList.add("button");
-        button.id = `b${i}`;
+        button.id = symbolArray[i-1];
         button.innerHTML = symbolArray[i-1];
+        if (parseInt(symbolArray[i-1]) || symbolArray[i-1] == "0"){
+            button.addEventListener("click", function() {changeScreen(button.id)});
+        }
+        else if (symbolArray[i-1] == "="){
+            //Give button complete math function here
+        }
+        else if (symbolArray[i-1] == "."){
+            //Decimal logic will go here
+        }
+        else {
+            //Giving buttons operand function will go here
+        }
     }
+}
+
+function changeScreen(value){
+    SCREEN.value = SCREEN.value + value;
 }
